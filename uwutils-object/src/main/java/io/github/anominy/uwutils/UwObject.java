961493de883de381e5a36f7e580027fb6c7ad4ad
anominy <140955338+anominy@users.noreply.github.com>
2024-10-02 17:15:42 +0300

    Add javadocs to uwutils/object module

--- uwutils-object/src/main/java/io/github/anominy/uwutils/UwObject.java
+++ uwutils-object/src/main/java/io/github/anominy/uwutils/UwObject.java
@@ -23,9 +23,25 @@ import org.jetbrains.annotations.UnknownNullability;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+/**
+ * An object utility class.
+ */
 @SuppressWarnings({"unused", "DefaultAnnotationParam"})
 public final class UwObject {
 
+    /**
+     * Apply provided object to a function if object and function aren't null.
+     * And return a default value if resulting one is null.
+     *
+     * @param object        object to apply, may be null
+     * @param function      function to invoke, may be null
+     * @param defaultValue  default value to return on failure, may be null
+     *
+     * @return  result of function apply or the default value
+     *
+     * @param <T>   object type
+     * @param <R>   return type
+     */
     @UnknownNullability
     @Contract(value = "null, _, _ -> param3; _, null, _ -> param3; _, _, !null -> !null", pure = false)
     public static <T, R> R ifNotNull(
@@ -45,6 +61,19 @@ public final class UwObject {
         return ifNull(function.apply(object), defaultValue);
     }
 
+    /**
+     * Apply provided object to a function if object and function aren't null.
+     * And return a default value if resulting one is null.
+     *
+     * @param object                object to apply, may be null
+     * @param function              function to invoke, may be null
+     * @param defaultValueSupplier  supplier to get the default value from on failure, may be null
+     *
+     * @return  result of function apply or the default value
+     *
+     * @param <T>   object type
+     * @param <R>   return type
+     */
     @UnknownNullability
     @Contract(value = "null, _, null -> null; _, null, null -> null", pure = false)
     public static <T, R> R ifNotNull(
@@ -64,6 +93,21 @@ public final class UwObject {
         return ifNull(function.apply(object), defaultValueSupplier);
     }
 
+    /**
+     * Apply provided object to a function if object and function aren't null.
+     * And return a default value if resulting one is null.
+     *
+     * <p>Wraps {@link #ifNotNull(Object, Function, Supplier)}.
+     *
+     * @param object                object to apply, may be null
+     * @param function              function to invoke, may be null
+     * @param defaultValueSupplier  supplier to get the default value from on failure, may be null
+     *
+     * @return  result of function apply or the default value
+     *
+     * @param <T>   object type
+     * @param <R>   return type
+     */
     @UnknownNullability
     @Contract(value = "null, _, _ -> null; _, null, _ -> null", pure = false)
     public static <T, R> R ifNotNull(
@@ -79,6 +123,20 @@ public final class UwObject {
         return ifNotNull(object, function, (Supplier<@UnknownNullability R>) defaultValueSupplier);
     }
 
+    /**
+     * Apply provided object to a function if object and function aren't null.
+     *
+     * <p>Wraps {@link #ifNotNull(Object, Function, Object)}
+     * w/ {@code null} as the default value.
+     *
+     * @param object    object to apply, may be null
+     * @param function  function to invoke, may be null
+     *
+     * @return  result of function apply or {@code null}
+     *
+     * @param <T>   object type
+     * @param <R>   return type
+     */
     @UnknownNullability
     @Contract(value = "null, _ -> null; _, null -> null", pure = false)
     public static <T, R> R ifNotNull(
@@ -91,6 +149,26 @@ public final class UwObject {
         return ifNotNull(object, function, (@Nullable R) null);
     }
 
+    /**
+     * Apply provided object to a function if object isn't null
+     * or return a default value.
+     *
+     * <p>Possible failure cases:
+     * <ul>
+     *     <li>Provided object is null.</li>
+     * </ul>
+     *
+     * @param object        object to apply, may be null
+     * @param function      function to invoke, mustn't be null
+     * @param defaultValue  default value to return on failure, may be null
+     *
+     * @return  result of function apply or the default value
+     *
+     * @throws NullPointerException if provided function is {@code null} and object isn't
+     *
+     * @param <T>   object type
+     * @param <R>   return type
+     */
     @UnknownNullability
     @Contract(value = "null, _, _ -> param3; !null, null, _ -> fail", pure = false)
     public static <T, R> R ifNotNullNoCheck(
@@ -110,6 +188,27 @@ public final class UwObject {
         return function.apply(object);
     }
 
+    /**
+     * Apply provided object to a function if object isn't null
+     * or return a default value.
+     *
+     * <p>Possible failure cases:
+     * <ul>
+     *     <li>Provided object is null.</li>
+     * </ul>
+     *
+     * @param object                object to apply, may be null
+     * @param function              function to invoke, mustn't be null
+     * @param defaultValueSupplier  supplier to get the default value from on failure, mustn't be null
+     *
+     * @return  result of function apply or the default value
+     *
+     * @throws NullPointerException if provided object and default value supplier are {@code null},
+     *                              if provided function is {@code null} and object isn't
+     *
+     * @param <T>   object type
+     * @param <R>   return type
+     */
     @UnknownNullability
     @Contract(value = "null, _, null -> fail; !null, null, _ -> fail", pure = false)
     public static <T, R> R ifNotNullNoCheck(
@@ -129,6 +228,29 @@ public final class UwObject {
         return function.apply(object);
     }
 
+    /**
+     * Apply provided object to a function if object isn't null
+     * or return a default value.
+     *
+     * <p>Wraps {@link #ifNotNullNoCheck(Object, Function, Supplier)}.
+     *
+     * <p>Possible failure cases:
+     * <ul>
+     *     <li>Provided object is null.</li>
+     * </ul>
+     *
+     * @param object                object to apply, may be null
+     * @param function              function to invoke, mustn't be null
+     * @param defaultValueSupplier  supplier to get the default value from on failure, mustn't be null
+     *
+     * @return  result of function apply or the default value
+     *
+     * @throws NullPointerException if provided object and default value supplier are {@code null},
+     *                              if provided function is {@code null} and object isn't
+     *
+     * @param <T>   object type
+     * @param <R>   return type
+     */
     @UnknownNullability
     @Contract(value = "null, _, !null -> null; !null, null, _ -> fail", pure = false)
     public static <T, R> R ifNotNullNoCheck(
@@ -144,6 +266,28 @@ public final class UwObject {
         return ifNotNullNoCheck(object, function, (Supplier<@UnknownNullability R>) defaultValueSupplier);
     }
 
+    /**
+     * Apply provided object to a function if object isn't null
+     * or return {@code null} on failure.
+     *
+     * <p>Wraps {@link #ifNotNullNoCheck(Object, Function, Object)}
+     * w/ {@code null} as the default value.
+     *
+     * <p>Possible failure cases:
+     * <ul>
+     *     <li>Provided object is null.</li>
+     * </ul>
+     *
+     * @param object    object to apply, may be null
+     * @param function  function to invoke, mustn't be null
+     *
+     * @return  result of function apply or {@code null}
+     *
+     * @throws NullPointerException if provided function is {@code null} and object isn't
+     *
+     * @param <T>   object type
+     * @param <R>   return type
+     */
     @UnknownNullability
     @Contract(value = "null, _ -> null; !null, null -> fail", pure = false)
     public static <T, R> R ifNotNullNoCheck(
@@ -156,6 +300,17 @@ public final class UwObject {
         return ifNotNullNoCheck(object, function, (@Nullable R) null);
     }
 
+    /**
+     * Return provided object if it isn't null
+     * or return a default value.
+     *
+     * @param object    object to check, may be null
+     * @param value     default value to return, may be null
+     *
+     * @return  provided object or if it's null the default value
+     *
+     * @param <T>   object type
+     */
     @UnknownNullability
     @Contract(value = "null, _ -> param2; !null, _ -> param1", pure = true)
     public static <T> T ifNull(
@@ -172,6 +327,17 @@ public final class UwObject {
         return object;
     }
 
+    /**
+     * Return provided object if it isn't null
+     * or return a default value.
+     *
+     * @param object    object to check, may be null
+     * @param supplier  supplier to get the default value, may be null
+     *
+     * @return  provided object or if it's null the default value
+     *
+     * @param <T>   object type
+     */
     @UnknownNullability
     @Contract(value = "null, null -> null; !null, _ -> param1", pure = false)
     public static <T> T ifNull(
@@ -188,6 +354,19 @@ public final class UwObject {
         return object;
     }
 
+    /**
+     * Return provided object if it isn't null
+     * or return a default value.
+     *
+     * @param object    object to check, may be null
+     * @param supplier  supplier to get the default value from, mustn't be null
+     *
+     * @return  provided object or if it's null the default value
+     *
+     * @throws NullPointerException if provided object and supplier are {@code null}
+     *
+     * @param <T>   object type
+     */
     @UnknownNullability
     @Contract(value = "!null, _ -> param1; null, null -> fail", pure = false)
     public static <T> T ifNullNoCheck(
@@ -204,6 +383,21 @@ public final class UwObject {
         return object;
     }
 
+    /**
+     * Return provided object if it isn't null
+     * or return a default value.
+     *
+     * <p>Wraps {@link #ifNullNoCheck(Object, Supplier)}.
+     *
+     * @param object    object to check, may be null
+     * @param supplier  supplier to get the default value from, mustn't be null
+     *
+     * @return  provided object or if it's null the default value
+     *
+     * @throws NullPointerException if provided object and supplier are {@code null}
+     *
+     * @param <T>   object type
+     */
     @UnknownNullability
     @Contract(value = "null, !null -> null; !null, _ -> param1; null, null -> fail", pure = false)
     public static <T> T ifNullNoCheck(
