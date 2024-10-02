4b605536b168c0c0a400922e626a19e7e21b7d4e
anominy <140955338+anominy@users.noreply.github.com>
2024-10-02 16:03:09 +0300

    Add javadocs to uwutils/array module

--- uwutils-array/src/main/java/io/github/anominy/uwutils/UwArray.java
+++ uwutils-array/src/main/java/io/github/anominy/uwutils/UwArray.java
@@ -24,9 +24,34 @@ import org.jetbrains.annotations.UnknownNullability;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
+/**
+ * An array utility class.
+ */
 @SuppressWarnings({"unused", "DefaultAnnotationParam"})
 public final class UwArray {
 
+    /**
+     * Get an element from an array by its index
+     * or return a default value if the index is out of range
+     * or resulting element is null.
+     *
+     * <p>Wraps {@link #getNoCheck(Object[], int)}.
+     *
+     * <p>Possible failure cases:
+     * <ul>
+     *     <li>Provided array is null.</li>
+     *     <li>Provided index is null.</li>
+     *     <li>Resulting element is null.</li>
+     * </ul>
+     *
+     * @param array         array to get element from, may be null
+     * @param index         index of element to get, may be null
+     * @param defaultValue  default value to return on failure, may be null
+     *
+     * @return  element corresponding to its index or the default value
+     *
+     * @param <T>   element type
+     */
     @UnknownNullability
     @Contract(value = "null, _, _ -> param3; _, null, _ -> param3; _, _, !null -> !null", pure = true)
     public static <T> T getOrElse(
@@ -46,6 +71,28 @@ public final class UwArray {
         return UwObject.ifNull(getNoCheck(array, index), defaultValue);
     }
 
+    /**
+     * Get an element from an array by its index
+     * or return a default value if the index is out of range
+     * or resulting element is null.
+     *
+     * <p>Wraps {@link #getOrNull(Object[], Integer)}.
+     *
+     * <p>Possible failure cases:
+     * <ul>
+     *     <li>Provided array is null.</li>
+     *     <li>Provided index is null.</li>
+     *     <li>Resulting element is null.</li>
+     * </ul>
+     *
+     * @param array                 array to get element from, may be null
+     * @param index                 index of element to get, may be null
+     * @param defaultValueSupplier  supplier to get the default value from on failure, may be null
+     *
+     * @return  element corresponding to its index or the default value
+     *
+     * @param <T>   element type
+     */
     @UnknownNullability
     @Contract(pure = false)
     public static <T> T getOrElse(
@@ -61,6 +108,28 @@ public final class UwArray {
         return UwObject.ifNull(getOrNull(array, index), defaultValueSupplier);
     }
 
+    /**
+     * Get an element from an array by its index
+     * or return a default value if the index is out of range
+     * or resulting element is null.
+     *
+     * <p>Wraps {@link #getOrElse(Object[], Integer, Supplier)}.
+     *
+     * <p>Possible failure cases:
+     * <ul>
+     *     <li>Provided array is null.</li>
+     *     <li>Provided index is null.</li>
+     *     <li>Resulting element is null.</li>
+     * </ul>
+     *
+     * @param array                 array to get element from, may be null
+     * @param index                 index of element to get, may be null
+     * @param defaultValueSupplier  supplier to get the default value from on failure, may be null
+     *
+     * @return  element corresponding to its index or the default value
+     *
+     * @param <T>   element type
+     */
     @UnknownNullability
     @Contract(value = "null, _, _ -> null; _, null, _ -> null", pure = false)
     public static <T> T getOrElse(
@@ -76,6 +145,27 @@ public final class UwArray {
         return getOrElse(array, index, (Supplier<@UnknownNullability T>) defaultValueSupplier);
     }
 
+    /**
+     * Get an element from an array by its index
+     * or return {@code null} if the index is out of range.
+     *
+     * <p>Wraps {@link #getOrElse(Object[], Integer, Object)}
+     * w/ {@code null} as the default value.
+     *
+     * <p>Possible failure cases:
+     * <ul>
+     *     <li>Provided array is null.</li>
+     *     <li>Provided index is null.</li>
+     *     <li>Resulting element is null.</li>
+     * </ul>
+     *
+     * @param array     array to get element from, may be null
+     * @param index     index of element to get, may be null
+     *
+     * @return  element corresponding to its index or {@code null}
+     *
+     * @param <T>   element type
+     */
     @UnknownNullability
     @Contract(value = "null, _ -> null; _, null -> null", pure = true)
     public static <T> T getOrNull(
@@ -87,7 +177,24 @@ public final class UwArray {
     ) {
         return getOrElse(array, index, (@Nullable T) null);
     }
-
+    /**
+     * Get an element from an array by its index
+     * or return {@code null} if the index is out of range.
+     *
+     * <p>Possible failure cases:
+     * <ul>
+     *     <li>Provided array is null.</li>
+     * </ul>
+     *
+     * @param array array to get element from, mustn't be null
+     * @param index index of element to get, mustn't be null
+     *
+     * @return  element corresponding to its index or {@code null}
+     *
+     * @throws NullPointerException if provided array is {@code null}
+     *
+     * @param <T>   element type
+     */
     @UnknownNullability
     @Contract(value = "null, _ -> fail", pure = true)
     public static <T> T getNoCheck(
@@ -105,6 +212,24 @@ public final class UwArray {
         return array[index];
     }
 
+    /**
+     * Propagate an item to first null element of an array.
+     *
+     * <p>Wraps {@link #propagateNoCheck(Object[], Object)}.
+     *
+     * <p>Possible failure cases:
+     * <ul>
+     *     <li>Provided array is null.</li>
+     * </ul>
+     *
+     * @param array     array to propagate the item in, may be null
+     * @param item      item to propagate in the array, may be null
+     *
+     * @return  {@code true} if successfully propagated an item,
+     *          {@code false} if failed to propagate an item
+     *
+     * @param <T>   element type
+     */
     @Contract(value = "null, _ -> false; _, null -> false", pure = false)
     public static <T> boolean propagate(
             @Nullable
@@ -120,6 +245,19 @@ public final class UwArray {
         return propagateNoCheck(array, item);
     }
 
+    /**
+     * Propagate an item to first null element of an array.
+     *
+     * @param array     array to propagate the item in, mustn't be null
+     * @param item      item to propagate in the array, may be null
+     *
+     * @return  {@code true} if successfully propagated an item,
+     *          {@code false} if failed to propagate an item
+     *
+     * @throws NullPointerException if provided array is {@code null}
+     *
+     * @param <T>   element type
+     */
     @Contract(value = "null, _ -> fail", pure = false)
     public static <T> boolean propagateNoCheck(
             @Nullable
@@ -140,6 +278,25 @@ public final class UwArray {
         return false;
     }
 
+    /**
+     * Consume first non-null elements of an array by passing them to a consumer.
+     *
+     * <p>Wraps {@link #consumeNoCheck(Object[], Consumer)}.
+     *
+     * <p>Possible failure cases:
+     * <ul>
+     *     <li>Provided array is null.</li>
+     *     <li>Provided consumer is null.</li>
+     * </ul>
+     *
+     * @param array     array of elements to consume, may be null
+     * @param consumer  consumer to pass elements to, may be null
+     *
+     * @return  {@code true} if successfully consumed at least one item,
+     *          {@code false} if failed to consume at least one item
+     *
+     * @param <T>   element type
+     */
     @Contract(value = "null, _ -> false; _, null -> false", pure = false)
     public static <T> boolean consume(
             @Nullable
@@ -155,6 +312,19 @@ public final class UwArray {
         return consumeNoCheck(array, consumer);
     }
 
+    /**
+     * Consume first non-null elements of an array by passing them to a consumer.
+     *
+     * @param array     array of elements to consume, mustn't be null
+     * @param consumer  consumer to pass elements to, mustn't be null
+     *
+     * @return  {@code true} if successfully consumed at least one item,
+     *          {@code false} if failed to consume at least one item
+     *
+     * @throws NullPointerException if provided array or consumer is {@code null}
+     *
+     * @param <T>   element type
+     */
     @Contract(value = "null, _ -> fail", pure = false)
     public static <T> boolean consumeNoCheck(
             @Nullable
@@ -177,6 +347,15 @@ public final class UwArray {
         return result;
     }
 
+    /**
+     * Create an iterable instance for an array.
+     *
+     * @param array     array to create an iterable instance for, may be null
+     *
+     * @return  new iterable instance
+     *
+     * @param <T>   element type
+     */
     @NotNull
     @Contract(value = "_ -> new", pure = true)
     public static <T> java.lang.Iterable<@UnknownNullability T> iterable(
@@ -186,6 +365,15 @@ public final class UwArray {
         return new Iterable<>(array);
     }
 
+    /**
+     * Create an iterator instance for an array.
+     *
+     * @param array     array to create an iterator instance for, may be null
+     *
+     * @return  new iterator instance
+     *
+     * @param <T>   element type
+     */
     @NotNull
     @Contract(value = "_ -> new", pure = true)
     public static <T> java.util.Iterator<@UnknownNullability T> iterator(
@@ -195,11 +383,24 @@ public final class UwArray {
         return new Iterator<>(array);
     }
 
+    /**
+     * Implementation of {@link java.lang.Iterable} interface for arrays.
+     *
+     * @param <T>   element type
+     */
     public static final class Iterable<T> implements java.lang.Iterable<@UnknownNullability T> {
 
+        /**
+         * An array.
+         */
         @Nullable
         private final T @Nullable [] array;
 
+        /**
+         * Initialize an iterable instance.
+         *
+         * @param array     array of elements, may be null
+         */
         @Contract(pure = true)
         public Iterable(
                 @Nullable
@@ -208,11 +409,20 @@ public final class UwArray {
             this.array = array;
         }
 
+        /**
+         * Initialize an iterable instance.
+         *
+         * <p>Wraps {@link #Iterable(Object[])}
+         * w/ {@code null} as the array.
+         */
         @Contract(pure = true)
         public Iterable() {
             this(null);
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         @Contract(pure = false)
         public void forEach(
@@ -226,6 +436,9 @@ public final class UwArray {
             java.lang.Iterable.super.forEach(action);
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         @NotNull
         @Contract(value = "-> new", pure = true)
@@ -234,13 +447,29 @@ public final class UwArray {
         }
     }
 
+    /**
+     * Implementation of {@link java.util.Iterator} interface for arrays.
+     *
+     * @param <T>   element type
+     */
     public static final class Iterator<T> implements java.util.Iterator<@UnknownNullability T> {
 
+        /**
+         * An array.
+         */
         @Nullable
         private final T @Nullable [] array;
 
+        /**
+         * An index.
+         */
         private int index;
 
+        /**
+         * Initialize an iterator instance.
+         *
+         * @param array     array of elements, may be null
+         */
         @Contract(pure = true)
         public Iterator(
                 @Nullable
@@ -250,11 +479,20 @@ public final class UwArray {
             this.index = -1;
         }
 
+        /**
+         * Initialize an iterator instance.
+         *
+         * <p>Wraps {@link #Iterator(Object[])}
+         * w/ {@code null} as the array.
+         */
         @Contract(pure = true)
         public Iterator() {
             this(null);
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         @Contract(pure = true)
         public boolean hasNext() {
@@ -262,6 +500,9 @@ public final class UwArray {
                     && this.index < this.array.length - 1;
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         @Nullable
         @Contract(pure = true)
@@ -269,6 +510,9 @@ public final class UwArray {
             return UwArray.getOrNull(this.array, ++this.index);
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         @Contract(pure = false)
         public void remove() {
@@ -281,6 +525,9 @@ public final class UwArray {
             this.array[this.index] = null;
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         @Contract(pure = false)
         public void forEachRemaining(
