bd68efcc3efbcf76c8c3e8c263dcbb3612202e73
anominy <140955338+anominy@users.noreply.github.com>
2024-10-02 16:46:14 +0300

    Add javadocs to uwutils/set module

--- uwutils-set/src/main/java/io/github/anominy/uwutils/UwSet.java
+++ uwutils-set/src/main/java/io/github/anominy/uwutils/UwSet.java
@@ -18,16 +18,33 @@ package io.github.anominy.uwutils;
 
 import org.jetbrains.annotations.*;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
 
+/**
+ * A set utility class.
+ */
 @SuppressWarnings({"unused", "DefaultAnnotationParam"})
 public final class UwSet {
 
+    /**
+     * An empty set instance.
+     */
     @NotNull
     @SuppressWarnings("rawtypes")
     public static final Set EMPTY = Collections.EMPTY_SET;
 
+    /**
+     * Check if provided set is unmodifiable.
+     *
+     * <p>Wraps {@link #isUnmodifiableNoCheck(Set)}.
+     *
+     * @param set   set to check for, may be null
+     *
+     * @return  {@code true} if is unmodifiable
+     *          and {@code false} if not
+     */
     @Contract(value = "null -> false", pure = false)
     public static boolean isUnmodifiable(
             @Nullable
@@ -40,6 +57,18 @@ public final class UwSet {
         return isUnmodifiableNoCheck(set);
     }
 
+    /**
+     * Check if provided set is unmodifiable.
+     *
+     * <p>Wraps {@link UwCollection#isUnmodifiableNoCheck(Collection)}.
+     *
+     * @param set  set to check for, mustn't be null
+     *
+     * @return  {@code true} if is unmodifiable
+     *          and {@code false} if not
+     *
+     * @throws NullPointerException if provided set is {@code null}
+     */
     @Contract(value = "null -> fail", pure = false)
     public static boolean isUnmodifiableNoCheck(
             @UnknownNullability
@@ -48,6 +77,17 @@ public final class UwSet {
         return UwCollection.isUnmodifiableNoCheck(set);
     }
 
+    /**
+     * Create unmodifiable view of provided set.
+     *
+     * <p>Wraps {@link #toUnmodifiableNoCheck(Set)}.
+     *
+     * @param set  set to create unmodifiable view for, may be null
+     *
+     * @return  new unmodifiable {@link Set} instance or {@code null}
+     *
+     * @param <T>   element type
+     */
     @UnknownNullability
     @UnmodifiableView
     @Contract(value = "null -> null; !null -> !null", pure = false)
@@ -62,6 +102,19 @@ public final class UwSet {
         return toUnmodifiableNoCheck(set);
     }
 
+    /**
+     * Create unmodifiable view of provided set.
+     *
+     * <p>Wraps {@link Collections#unmodifiableSet(Set)}.
+     *
+     * @param set  set to create unmodifiable view for, mustn't be null
+     *
+     * @return  new unmodifiable {@link Set} instance
+     *
+     * @throws NullPointerException if provided set is {@code null}
+     *
+     * @param <T>   element type
+     */
     @NotNull
     @UnmodifiableView
     @Contract(value = "null -> fail", pure = false)
